ecba56a24d4c7e68a570a0b03ad9489377dd91e7
mayhembyte <luizaandriani98@gmail.com>
2025-05-23 13:24:15 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,7 +4,8 @@ let transitionTimer = 0;
 let mainCircleSize = 60;
 let circles = [];
 let particles = [];
-let separationProgress = 0;
+let maxOffset = 150;
+let hasExploded = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -36,6 +37,7 @@ function draw() {
     }
   } else if (state === 'sellside') {
     drawSellSide();
+    updateParticles();
   }
 }
 
@@ -52,14 +54,13 @@ function drawIntro() {
 
 function drawSellSide() {
   transitionTimer++;
-  let maxOffset = 150;
   let offset = easeOutQuad(min(1, transitionTimer / 120)) * maxOffset;
 
   let leftX = width / 2 - offset;
   let rightX = width / 2 + offset;
   let y = height / 2;
 
-  // Efeito de bolha fluido entre os dois círculos
+  // Efeito fluido entre os dois círculos
   for (let i = -mainCircleSize; i <= mainCircleSize; i += 6) {
     let inter = map(i, -mainCircleSize, mainCircleSize, 0, 1);
     let x = lerp(leftX, rightX, 0.5) + sin(frameCount * 0.08 + i * 0.1) * 4;
@@ -68,13 +69,55 @@ function drawSellSide() {
     ellipse(x, yOff, 2, 2);
   }
 
-  // Os dois círculos principais
+  // Círculos principais
   fill(255, 180);
   ellipse(leftX, y, mainCircleSize);
   ellipse(rightX, y, mainCircleSize);
+
+  // Iniciar partículas quando a separação estiver completa
+  if (offset >= maxOffset && !hasExploded) {
+    for (let i = 0; i < 60; i++) {
+      particles.push(new Particle(width / 2, height / 2));
+    }
+    hasExploded = true;
+  }
+}
+
+function updateParticles() {
+  for (let i = particles.length - 1; i >= 0; i--) {
+    particles[i].update();
+    particles[i].show();
+    if (particles[i].alpha <= 0) {
+      particles.splice(i, 1);
+    }
+  }
+}
+
+class Particle {
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    let angle = random(TWO_PI);
+    let speed = random(1, 3);
+    this.vel = p5.Vector.fromAngle(angle).mult(speed);
+    this.alpha = 255;
+    this.r = random(1.5, 3.5);
+  }
+
+  update() {
+    this.pos.add(this.vel);
+    this.vel.mult(0.98); // atrito
+    this.alpha -= 3;
+  }
+
+  show() {
+    noStroke();
+    fill(255, this.alpha);
+    ellipse(this.pos.x, this.pos.y, this.r);
+  }
 }
 
 function easeOutQuad(t) {
   return t * (2 - t);
 }
+
 
